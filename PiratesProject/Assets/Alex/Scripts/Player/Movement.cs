6b271cf0140fda335b41e3fd87a71325dac116e1
Alexander Miroshnichenko <a.miroshnichenko@student.saedubai.com>
2023-06-18 20:47:30 +0400

    Added player movement and controls for all player input controllers

---
+++ PiratesProject/Assets/Alex/Scripts/Player/Movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputHandler))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private InputHandler inputHandler;
+
+    private void Awake()
+    {
+        inputHandler = GetComponent<InputHandler>();
+        rb= GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MovementHandler();
+    }
+    
+    private void MovementHandler()
+    {
+        movement.x = Input.GetAxis(inputHandler.GetPlayerRole() + "Horizontal");
+        movement.y = Input.GetAxis(inputHandler.GetPlayerRole() + "Vertical");
+
+        rb.velocity = movement.normalized * moveSpeed;
+    }
+}
