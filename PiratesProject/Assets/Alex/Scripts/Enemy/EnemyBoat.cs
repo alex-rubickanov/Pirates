a60b9457ccdd103cedb380431bad44e1fab39adf
Alexander Miroshnichenko <a.miroshnichenko@student.saedubai.com>
2023-06-22 01:08:10 +0400

    Garbage

--- PiratesProject/Assets/Alex/Scripts/Enemy/EnemyBoat.cs
+++ PiratesProject/Assets/Alex/Scripts/Enemy/EnemyBoat.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBoat : MonoBehaviour
 {
@@ -8,15 +10,23 @@ public class EnemyBoat : MonoBehaviour
     [SerializeField] private float distanceToShoot;
     [SerializeField] private float moveSpeed;
     [SerializeField] private States state;
+    private Rigidbody2D rb;
+    [SerializeField] private float timeToWait;
     private enum States
     {
         Moving,
         Shooting
     }
 
-    private void Update()
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
     {
         state = IsOnLineWithPlayerBoat();
+        Debug.Log(state);
 
         switch (state) {
             case States.Moving:
@@ -30,23 +40,18 @@ public class EnemyBoat : MonoBehaviour
 
     private void Move()
     {
-        if(transform.position.y != playerBoat.transform.position.y) {
-            if(transform.position.y > playerBoat.transform.position.y) {
-                transform.position -= Time.deltaTime * Vector3.up * moveSpeed;
-            } else if (transform.position.y < playerBoat.transform.position.y) {
-                transform.position += Time.deltaTime * Vector3.up * moveSpeed;
-            }
-        }
+        var step = moveSpeed * Time.deltaTime;
+        rb.velocity = -Vector3.MoveTowards(transform.position, playerBoat.transform.position, step);
     }
 
     private void Shoot()
     {
-        Debug.Log("Shoot");
+        rb.velocity = Vector3.zero;
     }
 
     private States IsOnLineWithPlayerBoat()
     {
-        if(transform.position.y == playerBoat.transform.position.y) {
+        if(Vector3.Distance(transform.position, playerBoat.transform.position) > distanceToShoot) {
             return States.Moving;
         } else {
             return States.Shooting;
